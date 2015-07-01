54298c88732621c05291f1294325c92d523058d4
Jin Choi <jsc@alum.mit.edu>
2015-07-01 11:20:00 -0400

    Added pause handling in gcode sender.

--- Utils/gcode-sender.rb
+++ Utils/gcode-sender.rb
@@ -5,21 +5,27 @@ require 'serialport'
 filename = ARGV.shift
 
 
-SerialPort.open("/dev/cu.usbmodem1411", 115200, 8, 1, SerialPort::NONE) do |sp|
+SerialPort.open("/dev/cu.usbmodem145311", 115200, 8, 1, SerialPort::NONE) do |sp|
   while line = sp.gets
     print line
     break if line.match(/^Ready/)
   end
   File.foreach(filename) do |line|
     line.chomp!
-    line.gsub!(/\(.*\)/, '')
-    line.gsub!(/;.*/, '')
+    line.gsub!(/\s*\(.*\)/, '')
+    line.gsub!(/\s*;.*/, '')
     if !line.empty?
-      puts line
+      # puts line
+
+      if line == "M01"
+        puts "Pausing. Press Enter to continue."
+        gets
+        next
+      end
       sp.puts line
 
       while line = sp.gets
-        print "\t#{line}"
+        # print "\t#{line}"
         break if line.match(/^ok:/)
       end
     end
