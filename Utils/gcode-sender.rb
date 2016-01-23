c07ccc2aea3043724d4b9e7f0b77d637c8fa803f
Jin Choi <jsc@alum.mit.edu>
2016-01-23 17:37:55 -0500

    Made gcode-sender.rb work under Windows.

--- Utils/gcode-sender.rb
+++ Utils/gcode-sender.rb
@@ -1,11 +1,33 @@
 #!/usr/bin/env ruby
+# Copyright 2015 Jin Choi (jsc@alum.mit.edu)
+
+# Under Windows:
+# Download and install Ruby using rubyinstaller:
+# http://rubyinstaller.org/downloads/
+# Also download and unpack a DevKit-mingw from that same page.
+# I recommend unpacking it into a new directory.
+
+# Open a command window (Windows-r, cmd), cd to the directory you
+# unpacked the DevKit. Run "devkitvars.bat".
+
+# Make sure you have the Arduino device drivers installed, by
+# installing the Arduino IDE from arduino.cc.
+
+# For all operating systems:
+# Run "gem install serialport".
+# Edit the port variable below to be the port name of your SphereBot.
+
+# To use: ruby /path/to/gcode-sender.rb file.gcode
 
 require 'serialport'
 
+port = "/dev/cu.usbmodem145311"
+# port = "COM3"
+
 filename = ARGV.shift
 
-
-SerialPort.open("/dev/cu.usbmodem145311", 115200, 8, 1, SerialPort::NONE) do |sp|
+SerialPort.open(port, 115200, 8, 1, SerialPort::NONE) do |sp|
+  sp.read_timeout = 0 # Necessary for Windows.
   while line = sp.gets
     print line
     break if line.match(/^Ready/)
