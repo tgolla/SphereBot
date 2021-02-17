0c7b113468347d438891a47a90adc434e1f89bf5
Terence Golla <tgolla@outlook.com>
2021-02-17 09:27:24 -0600

    Moved GCodeParser files.

--- GCodeParser/GCodeParserUnitTests/GCodeParserUnitTests.cpp
+++ GCodeParser/GCodeParserUnitTests/GCodeParserUnitTests.cpp
@@ -1,6 +1,6 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-#include "../GCodeParser/GCodeParser.h"
+#include "../../GCodeParser.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
